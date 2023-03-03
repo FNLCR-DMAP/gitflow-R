7f667d85826c28b5d70b7cf8412e4e332be177cd
Rui He <her2@nih.gov>
2023-03-03 15:01:13 -0500

    Reorganized the repo to serve as a source of reusable workflows, and updated workflow actions to adapt to external workflow calls

--- Check_On_Merge_Main.sh
+++ Check_On_Merge_Main.sh
@@ -8,8 +8,7 @@ current_dir="$1"
 if [ -f DESCRIPTION ]; then
     echo "DESCRIPTION exist."
     
-    Rscript -e 'if(! require("devtools")){install.packages("devtools")};'
-    Rscript -e 'library(devtools);sink(file="'${current_dir}'/test.log");load_all();test_dir("'${current_dir}/tests/integrated_test'");check();sink()'  
+    R -e '.libPaths(c("/renv/library/R-4.1/x86_64-pc-linux-gnu",.libPaths()));library(devtools);sink(file="'${current_dir}'/test.log");load_all();document();check();sink()'  
     
     cat test.log
     
@@ -17,27 +16,30 @@ if [ -f DESCRIPTION ]; then
     
     message_check=$(tail -n 1 test.log | cut -d'|' -f 1 | cut -d' ' -f 2)
     echo "Message Check: $message_check"
-    
-    if [ "$message_check" = "errors" ]; then
-      error_num=$(tail -n 1 test.log | cut -d'|' -f 1 | cut -d' ' -f 1)
-      if [ "$error_num" != "0" ]; then
-        echo "Number of errors in test is $error_num"
-        exit 2
-      else
-        echo "Passed Check!"
-      fi
-      
+    if [ "$message_check" = "FAIL" ]; then
+      echo "Failed integration test, please check the log."
+      exit 2
     else
-      error_num=$(tail -n3 test.log | head -n1 | cut -d'|' -f 1 | cut -d' ' -f 1)
-      
-      if [ "$error_num" != "0" ]; then
-        echo "Number of errors in test is $error_num"
-        exit 2
+      if [ "$message_check" = "errors" ]; then
+        error_num=$(tail -n 1 test.log | cut -d'|' -f 1 | cut -d' ' -f 1)
+        if [ "$error_num" != "0" ]; then
+          echo "Number of errors in test is $error_num"
+          exit 2
+        else
+          echo "Passed Check!"
+        fi
+        
       else
-        echo "Passed Check!"
+        error_num=$(tail -n3 test.log | head -n1 | cut -d'|' -f 1 | cut -d' ' -f 1)
+        
+        if [ "$error_num" != "0" ]; then
+          echo "Number of errors in test is $error_num"
+          exit 2
+        else
+          echo "Passed Check!"
+        fi
       fi
     fi
-    
 else 
     echo "DESCRIPTION file does not exist."
     exit 1
--- local_config.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -l
-
-export DOCKER_CONTAINER_ID="ruihe864/ccbr-github-playground:latest"
