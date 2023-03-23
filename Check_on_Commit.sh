229a2bc84782b00fa3c31a695501f2d9ce0f247a
Rui He <her2@nih.gov>
2023-03-23 12:34:27 -0400

    Update the template to python check for single-cell-analysis package CI support

--- Check_on_Commit.sh
+++ Check_on_Commit.sh
@@ -8,75 +8,52 @@ current_dir="$1"
 current_branch="$(git rev-parse --abbrev-ref HEAD)"
 echo "Checking latest push to $current_branch"
 
+R_script_test=($(git diff "$last_commit" HEAD --name-only $current_branch | \
+                grep -E 'tests/' | grep -v '/fixtures' | \
+                sed 's:.*/::' | grep -v '^helper-.*.py$'))
+                
+echo -e "Test script changed: \n${R_script_test[*]}\n"
 
-if [ -f DESCRIPTION ]; then
-    echo "DESCRIPTION exist."
-    
-    
-    R_script_test=($(git diff "$last_commit" HEAD --name-only $current_branch | \
-                    grep -E 'tests/testthat' | grep -v '/fixtures' | \
-                    sed 's:.*/::' | grep -v '^helper-.*.R$'))
-                    
-    echo -e "Test script changed: \n${R_script_test[*]}\n"
-    
-    R_script_func=($(git diff "$last_commit" HEAD --name-only $current_branch | \
-                    grep -E 'R/' | sed 's:.*/::' | grep -iE '*.R$'))
-                    
-    echo -e "Function script changed: \n${R_script_func[*]}\n"
-    
-    for R_script in "${R_script_func[@]}"
-    do
-      test_file=$(ls tests/testthat | grep -iE "$R_script" | grep -iE "test")
-      if [[ ! " ${R_script_test[*]} " =~ " ${test_file} " ]]; then
-        R_script_test+=("$test_file")
-      fi
-    done
-    
-    echo -e "Tests to run as: \n${R_script_test[*]}\n"
-    
-    R -e '.libPaths(c("/renv/library/R-4.1/x86_64-pc-linux-gnu",.libPaths()));.libPaths();ip = as.data.frame(installed.packages()[,c(1,3:4)]);ip = ip[is.na(ip$Priority),1:2,drop=FALSE];ip;sessionInfo();'
-    
-    for test_to_run in "${R_script_test[@]}"
-    do 
-      
-      test_call='test_file("'"$current_dir"'/tests/testthat/'"$test_to_run"'");'
+R_script_func=($(git diff "$last_commit" HEAD --name-only $current_branch | \
+                grep -E 'src/scworkflow/' | sed 's:.*/::' | grep -iE '*.py$'))
+                
+echo -e "Function script changed: \n${R_script_func[*]}\n"
 
-      
-      echo "====================================================================="
-      echo "Running $test_call"
-      
-      R -e '.libPaths(c("/renv/library/R-4.1/x86_64-pc-linux-gnu",.libPaths()));library(devtools);sink(file="'"${current_dir}"'/test.log");load_all();'"$test_call"'sink()'  
-      
-      cat test.log
-      
-      echo "====================================================================="
-      echo "====================================================================="
-      
-      message_check=$(tail -n 1 test.log | cut -d'|' -f 4 | cut -d' ' -f 2)
-      echo "Message Check: $message_check"
-      
-      if [ "$message_check" = "PASS" ]; then
-        FAIL_num=$(tail -n 1 test.log | cut -d'|' -f 1 | cut -d' ' -f 3)
-        if [ "$FAIL_num" != "0" ]; then
-          echo "Number of FAIL in test is $FAIL_num"
-          exit 2
-        else
-          echo "Passed Check!"
-        fi
-        
-      else
-        FAIL_num=$(tail -n3 test.log | head -n1 | cut -d'|' -f 1 | cut -d' ' -f 3)
-        
-        if [ "$FAIL_num" != "0" ]; then
-          echo "Number of FAIL in test is $FAIL_num"
-          exit 2
-        else
-          echo "Passed Check!"
-        fi
-      fi
-    done
-else 
-    echo "DESCRIPTION file does not exist."
-    exit 1
-fi
+for R_script in "${R_script_func[@]}"
+do
+  test_file=$(ls tests/testthat | grep -iE "$R_script" | grep -iE "test")
+  if [[ ! " ${R_script_test[*]} " =~ " ${test_file} " ]]; then
+    R_script_test+=("$test_file")
+  fi
+done
+
+echo -e "Tests to run as: \n${R_script_test[*]}\n"
+
+poetry add --dev pytest
+
+for test_to_run in "${R_script_test[@]}"
+do 
+  
+  test_call='pytest tests/$test_to_run'
+
+  
+  echo "====================================================================="
+  echo "Running $test_call"
+  
+  echo $(pytest tests/$test_to_run) > ${current_dir}/python_test_log.log
+  
+  cat python_test_log.log
+  
+  echo "====================================================================="
+  echo "====================================================================="
+  
+  message_check=$(cat python_test_log.log | grep -E "FAILED tests/")
+
+  if [ ! -z "$message_check" ]; then
+    echo "Failed Check!"
+    exit 2
+  else
+    echo "Passed Check!"
+  fi
+done
 
--- Lint_on_Commit.sh
+++ Lint_on_Commit.sh
@@ -8,47 +8,33 @@ current_dir="$1"
 current_branch="$(git rev-parse --abbrev-ref HEAD)"
 echo "Checking latest push to $current_branch"
 
+R_script_test=($(git diff "$last_commit" HEAD --name-only $current_branch | \
+                grep -E 'tests/'))
+                
+echo -e "Test script changed: \n${R_script_test[*]}\n"
 
-if [ -f DESCRIPTION ]; then
-    echo "DESCRIPTION exist."
-    
-    
-    R_script_test=($(git diff "$last_commit" HEAD --name-only $current_branch | \
-                    grep -E 'tests/testthat'))
-                    
-    echo -e "Test script changed: \n${R_script_test[*]}\n"
-    
-    R_script_func=($(git diff "$last_commit" HEAD --name-only $current_branch | \
-                    grep -E 'R/'))
-                    
-    echo -e "Function script changed: \n${R_script_func[*]}\n"
-    
-    
-    Lint_list=( "${R_script_test[@]}" "${R_script_func[@]}" )
-    
-    echo -e "Lint to run as: \n${R_script_test[*]}\n"
-    
-    for test_to_run in "${Lint_list[@]}"
-    do 
-      
-      test_call='File_to_lint = "'"$test_to_run"'";source("gitflow-R/Workflow_linter.R", local=FALSE, print.eval=TRUE);'
-      
-      
-      echo "====================================================================="
-      echo "Running $test_call"
-      
-      R -e '.libPaths(c("/renv/library/R-4.1/x86_64-pc-linux-gnu",.libPaths()));library(lintr);sink(file="'"${current_dir}"'/lint.log");'"$test_call"'sink()'  
-      
-      cat lint.log
-      
-      echo "====================================================================="
-      echo "====================================================================="
-    done
-      
-else 
+R_script_func=($(git diff "$last_commit" HEAD --name-only $current_branch | \
+                grep -E 'src/scworkflow/'))
+                
+echo -e "Function script changed: \n${R_script_func[*]}\n"
 
-    echo "DESCRIPTION file does not exist."
-    exit 1
-    
-fi
+
+Lint_list=( "${R_script_test[@]}" "${R_script_func[@]}" )
+
+echo -e "Lint to run as: \n${R_script_test[*]}\n"
+
+for test_to_run in "${Lint_list[@]}"
+do 
+  
+  echo "====================================================================="
+
+  echo $(pylint $test_to_run} > ${current_dir}/lint.log
+  
+  cat lint.log
+
+  
+  echo "====================================================================="
+  echo "====================================================================="
+done
+
 
