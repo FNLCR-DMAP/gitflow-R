37cae81779f8d1625761a4f536e48e1d782947c1
ruiheesi <107956586+ruiheesi@users.noreply.github.com>
2023-05-11 09:56:53 -0400

    Update Lint_on_Commit.sh

--- Lint_on_Commit.sh
+++ Lint_on_Commit.sh
@@ -14,7 +14,7 @@ R_script_test=($(git diff "$last_commit" HEAD --name-only $current_branch | \
 echo -e "Test script changed: \n${R_script_test[*]}\n"
 
 R_script_func=($(git diff "$last_commit" HEAD --name-only $current_branch | \
-                grep -E 'src/scworkflow/'))
+                grep -E 'src/spac/'))
                 
 echo -e "Function script changed: \n${R_script_func[*]}\n"
 
