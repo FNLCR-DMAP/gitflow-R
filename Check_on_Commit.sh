c40cde08d0d18b75559af224dedb12b0fa15efd1
ruiheesi <107956586+ruiheesi@users.noreply.github.com>
2023-05-12 14:44:44 -0400

    Added feature to acquire changed functions

--- Check_on_Commit.sh
+++ Check_on_Commit.sh
@@ -13,55 +13,96 @@ echo "Latest commit hash is $last_commit"
 
 pytest --version
 
-test_scripts=($(git diff "$last_commit" HEAD --name-only $current_branch | \
+test_scripts=($(git diff "$last_commit" HEAD \
+                --name-only $current_branch | \
                 grep -E 'tests/' | grep -v '/fixtures' | \
-                sed 's:.*/::' | grep -v '^helper-.*.py$'))
-                
+                sed 's:.*/::' | grep -v "__init__.py" | \
+                grep -v '^helper-.*.py$'))
+
+test_script_paths=($(git diff "$last_commit" HEAD \
+                --name-only $current_branch | \
+                grep -E 'tests/' | grep -v '/fixtures' | \
+                grep -v "__init__.py" | grep -v '^helper-.*.py$'))
+
+
 echo -e "Test script changed: \n${test_scripts[*]}\n"
 
-function_scripts=($(git diff "$last_commit" HEAD --name-only $current_branch | \
-                grep -E 'src/spac/' | sed 's:.*/::' | grep -iE '*.py$'))
-                
+function_scripts=($(git diff "$last_commit" HEAD \
+                --name-only $current_branch | \
+                grep -E 'src/spac/' | sed 's:.*/::' | \
+                grep -v "__init__.py" | grep -iE '*.py$'))
+
+function_script_paths=($(git diff "$last_commit" HEAD \
+                --name-only $current_branch | \
+                grep -E 'src/spac/' | grep -v "__init__.py" \
+                | grep -iE '*.py$'))
+
 echo -e "Function script changed: \n${function_scripts[*]}\n"
 
-all_test_files=($(ls tests/ | grep -iE "test_"))
+function_updated=()
 
-for function_name in "${function_scripts[@]}"
+for script in "${function_script_paths[@]}"
 do
-  test_file=$(ls tests/testthat | grep -iE "$function_name" | grep -iE "test")
-  if [[ ! " ${R_script_test[*]} " =~ " ${test_file} " ]]; then
-    test_scripts+=("$test_file")
-  fi
+  mod_func_list=($(grep -E '^def\s+\w+\(' $script | cut -d ' ' -f 2 | cut -d '(' -f 1))
+  echo "Function changed in $script:"
+
+  for function in "${mod_func_list[@]}"
+  do
+    echo "$function"
+  done
+
+  function_updated=("${function_updated[@]}" "${mod_func_list[@]}")
 done
 
 
-echo -e "Tests to run as: \n${test_scripts[*]}\n"
+for function_name in "${function_updated[@]}"
+do
+  test_scripts_found=($(find tests/  -type f -name "test_$function_name*" ! -path "*/__*" -exec echo {} \;))
 
-# poetry add --dev pytest
-
-for test_to_run in "${all_test_files[@]}"
-do 
-  echo $test_to_run
-  test_call='pytest tests/'$test_to_run
-
-  
-  echo "====================================================================="
-  echo "Running $test_call"
-  
-  echo $(pytest tests/$test_to_run) > ${current_dir}/python_test_log.log
-  
-  cat python_test_log.log
-  
-  echo "====================================================================="
-  echo "====================================================================="
-  
-  message_check=$(cat python_test_log.log)
-
-  if [ -s python_test_log.log ] && [[ ! $message_check =~ "FAILED tests/" ]]; then
-      echo "Passed Check!"
+  if [ ${#test_scripts_found[@]} -gt 0 ]; then
+      if [[ ! " ${function_script_paths[*]} " =~ " ${test_scripts_found} " ]]; then
+        test_script_paths+=("$test_scripts_found")
+      else
+        echo "Test script also changed for function: $function_name"
+      fi
   else
-      echo "Failed Check!"
-      exit 2
+      echo "No test script for function: $function_name"
+  fi
+
+done
+
+test_set=($(printf "%s\n" "${test_script_paths[@]}" | sort -u))
+
+echo -e "Tests to run are: "
+for test in ${test_set[@]};
+do
+  echo $test
+done
+
+test_records=()
+
+for test_to_run in "${test_set[@]}"
+do 
+  echo "Testing: $test_to_run"
+  
+  echo "====================================================================="
+  
+  pytest $test_to_run
+  
+  echo "====================================================================="
+
+  pytest_exit_status=$?
+
+  if [ $pytest_exit_status -eq 0 ]; then
+      echo "Test passed"
+      test_records+=("$test_to_run : Passed. ")
+  else
+      echo "Test failed"
+      test_records+=("$test_to_run : Failed. ")
   fi
 done
 
+for record in "${test_records[@]}";
+do
+  echo $record
+done
