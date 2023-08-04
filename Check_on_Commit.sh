4270d58b6d73dbbd170edd39cc016cfa07a407bf
ruiheesi <107956586+ruiheesi@users.noreply.github.com>
2023-08-04 08:47:32 -0400

    perf(commit check): Factor in push on commit

--- Check_on_Commit.sh
+++ Check_on_Commit.sh
@@ -16,7 +16,9 @@ if [ -f DESCRIPTION ]; then
     R_script_test=($(git diff "$last_commit" HEAD --name-only $current_branch | \
                     grep -E 'tests/testthat' | grep -v '/fixtures' | \
                     grep -v '/_snaps' | grep -v 'helper-' | \
-                    sed 's:.*/::' | grep -v '^helper-.*.R$' | grep -v 'test-Integration_Test.R' ))
+                    sed 's:.*/::' | grep -v '^helper-.*.R$' | \
+                    grep -v 'test-Integration_Test.R' | \
+                    grep -iE '*.R'))
                     
     echo -e "Test script changed: \n${R_script_test[*]}\n"
     
